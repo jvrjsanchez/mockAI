1a2e275d488e7e7b260f922d3e470585c64b733b
Wesley Brown <wesleykebrown@yahoo.com>
2024-06-24 20:15:34 -0400

    Debugging changes

--- mock_ai/components/Header.tsx
+++ mock_ai/components/Header.tsx
@@ -1,17 +1,30 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import { useAuth0 } from '@auth0/auth0-react'
+import { Button } from '@/components/ui/button'
 
-import CustomButton from './CustomButton'
+import { House } from 'lucide-react'
 
 const Header = () => {
+  const { loginWithRedirect, logout, isAuthenticated } = useAuth0()
   return (
-        <header className='w-full absolute z-10'>
-            <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 py-4 px-6'>
-                <Link href='/' className='flex justify-center items-center'>
-                    <Image src='/mockAILogo.jpeg' alt='mockAI' width={118} height={18} />
-                </Link>
-
-                <CustomButton title='Sign In' containerStyles='text-white bg-primary-blue rounded-full min-w-[130px]' />
+        <header className='relative text-center z-10'>
+            <Link href='/' className='flex justify-center items-center'>
+                <Image src='/mockAILogo.jpeg' alt='mockAI' width={118} height={18} />
+            </Link>
+            <nav className='flex justify-between'>
+                <div className='absolute -top-5 right-5 flex space-x-2'>
+                    <Link href='/'>
+                        <House className='w-8 h-8 lg:w-10 lg:h-10 mx-auto text-blue-500 mt-10 border border-blue-500 p-2 rounded-md hover:opacity-50 cursor-pointer' />
+                    </Link>
+                    <div>
+                        {!isAuthenticated ? (
+                            <Button onClick={() => loginWithRedirect()} variant='outline' size='sm' className='mt-10'>Sign-In to Interview</Button>
+                        ) : (
+                            <Button onClick={() => logout({})} variant='outline' size='sm' className='mt-10'>Sign Out</Button>
+                        )}
+                    </div>
+                </div>
             </nav>
         </header>
   )
--- mock_ai/components/index.ts
+++ mock_ai/components/index.ts
@@ -1,10 +1,8 @@
-import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Header from './Header'
 import Footer from './Footer'
 
 export {
-  Hero,
   CustomButton,
   Header,
   Footer
