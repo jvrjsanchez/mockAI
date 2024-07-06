9db23d2bf4d667b6930e0e045db0ef35df8f5fbe
Tim Quattrochi <timq82@gmail.com>
2024-07-06 15:31:54 -0400

    add condition for pause durations to show 0 if empty.

--- mock_ai/components/FillerCount.tsx
+++ mock_ai/components/FillerCount.tsx
@@ -1,5 +1,5 @@
-import { FC } from 'react'
-import { Feedback } from '@/types'
+import { FC } from "react";
+import { Feedback } from "@/types";
 
 interface FillerCountProps {
   feedback: Feedback;
@@ -13,49 +13,51 @@ const FillerCount: FC<FillerCountProps> = ({ feedback }) => {
       </h2>
       <ul className="list-disc pl-5 space-y-2">
         <li className="text-gray-700">
-          Like:{' '}
+          Like:{" "}
           <span className="font-semibold">
             {feedback.filler_word_count.like}
           </span>
         </li>
         <li className="text-gray-700">
-          So:{' '}
+          So:{" "}
           <span className="font-semibold">
             {feedback.filler_word_count.so}
           </span>
         </li>
         <li className="text-gray-700">
-          Uh:{' '}
+          Uh:{" "}
           <span className="font-semibold">
             {feedback.filler_word_count.uh}
           </span>
         </li>
         <li className="text-gray-700">
-          Um:{' '}
+          Um:{" "}
           <span className="font-semibold">
             {feedback.filler_word_count.um}
           </span>
         </li>
         <li className="text-gray-700">
-          You know:{' '}
+          You know:{" "}
           <span className="font-semibold">
-            {feedback.filler_word_count['you know']}
+            {feedback.filler_word_count["you know"]}
           </span>
         </li>
         <li className="text-gray-700">
-          Long pauses:{' '}
+          Long pauses:{" "}
           <span className="font-semibold">
             {feedback.long_pauses}
           </span>
         </li>
         <li className="text-gray-700">
-          Pause durations:{' '}
+          Pause durations:{" "}
           <span className="font-semibold">
-            {feedback.pause_durations.join(', ')}
+            {feedback.pause_durations.length === 0
+              ? 0
+              : feedback.pause_durations.join(", ")}
           </span>
         </li>
       </ul>
     </div>
-  )
-}
-export default FillerCount
+  );
+};
+export default FillerCount;
