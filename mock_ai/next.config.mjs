4675112397971740cb003a56650fa93091a3d6a9
jvrjsanchez <javiersanchez.cna@gmail.com>
2024-08-16 21:27:17 -0500

    auth 0 fixes

--- mock_ai/next.config.mjs
+++ mock_ai/next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    ignoreBuildErrors: true
+    ignoreBuildErrors: true,
   },
   rewrites: async () => {
     return [
       {
         source: "/service/:path*",
-        destination: "http://localhost:3001/service/:path*",
+        destination:
+          process.env.NODE_ENV === "development"
+            ? "http://127.0.0.1:3001/service/:path*"
+            : "/flask_api/",
       },
     ];
   },
