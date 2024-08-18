79f9c1729523bdc653fb9cbc7b71af4598cac9b2
jvrjsanchez <javiersanchez.cna@gmail.com>
2024-08-18 18:10:51 -0500

    got rid of some comments

--- mock_ai/next.config.mjs
+++ mock_ai/next.config.mjs
@@ -12,8 +12,6 @@ const nextConfig = {
           process.env.NODE_ENV === "production"
             ? "/flask_api/"
             : "http://127.0.0.1:3001/service/:path*",
-
-        // Resource: https://vercel.com/docs/projects/environment-variables/system-environment-variables
       },
     ];
   },
