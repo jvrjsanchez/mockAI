25609858c7ff97f6b6f330eec198c5d1f5d1285c
jvrjsanchez <javiersanchez.cna@gmail.com>
2024-08-18 19:38:17 -0500

    added new to middleware

--- mock_ai/middleware.ts
+++ mock_ai/middleware.ts
@@ -1,45 +1,45 @@
-// import { NextRequest, NextResponse } from "next/server";
-// import { getSession } from "@auth0/nextjs-auth0/edge";
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@auth0/nextjs-auth0/edge";
 
-// export async function middleware(req: NextRequest) {
-//   const res = new NextResponse();
+export async function middleware(req: NextRequest) {
+  const res = new NextResponse();
 
-//   try {
-//     const { user } = (await getSession(req, res)) || {};
-//     const URL = process.env.NEXT_PUBLIC_VERCEL_URL
+  try {
+    const { user } = (await getSession(req, res)) || {};
+    const URL = process.env.NEXT_PUBLIC_VERCEL_URL
 
-//     if (user) {
-//       const response = await fetch(
-//         process.env.NEXT_PUBLIC_VERCEL_URL
-//         ? "http://localhost:3001/service/add_user"
-//         : "/service/add_user",
-//         {
-//           method: "POST",
-//           headers: {
-//             "Content-Type": "application/json",
-//           },
-//           cache: "force-cache",
-//           body: JSON.stringify({
-//             email: user.email,
-//           }),
-//         }
-//       );
+    if (user) {
+      const response = await fetch(
+        process.env.NEXT_PUBLIC_VERCEL_URL
+        ? "http://localhost:3001/service/add_user"
+        : "/service/add_user",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          cache: "force-cache",
+          body: JSON.stringify({
+            email: user.email,
+          }),
+        }
+      );
 
-//       const responseFromFlask = await response.json();
+      const responseFromFlask = await response.json();
 
-//       console.log("Response from Flask:", responseFromFlask);
+      console.log("Response from Flask:", responseFromFlask);
 
-//       if (responseFromFlask.message === "Request received") {
-//         return NextResponse.next();
-//       }
-//     }
-//     return NextResponse.next();
-//   } catch (error) {
-//     console.error("Error in middleware:", error);
-//     return NextResponse.error();
-//   }
-// }
+      if (responseFromFlask.message === "Request received") {
+        return NextResponse.next();
+      }
+    }
+    return NextResponse.next();
+  } catch (error) {
+    console.error("Error in middleware:", error);
+    return new NextResponse.error();
+  }
+}
 
-// export const config = {
-//   matcher: "/interview",
-// };
+export const config = {
+  matcher: "/interview",
+};
