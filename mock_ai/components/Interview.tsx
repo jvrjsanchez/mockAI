81e8c1190b642589473d8ad82f2c50784d42b6d6
Tim Quattrochi <timq82@gmail.com>
2024-09-04 15:02:07 -0400

    refactor: text black on input typing

--- mock_ai/components/Interview.tsx
+++ mock_ai/components/Interview.tsx
@@ -1,21 +1,25 @@
-"use client"
-import { useUser } from '@auth0/nextjs-auth0/client';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import VoiceRecorder from './VoiceRecorder';
-import VideoRecorder from './VideoRecorder';
-import AnalysisCard from './AnalysisCard';
-import Link from 'next/link';
+"use client";
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useState, useEffect } from "react";
+import axios from "axios";
+import VoiceRecorder from "./VoiceRecorder";
+import VideoRecorder from "./VideoRecorder";
+import AnalysisCard from "./AnalysisCard";
+import Link from "next/link";
 
 const Interview = () => {
   const { user, error, isLoading } = useUser();
   const [step, setStep] = useState(1); // Step counter to control the flow
-  const [selectedQuestion, setSelectedQuestion] = useState<string | null>(null);
-  const [name, setName] = useState('');
-  const [company, setCompany] = useState('');
-  const [position, setPosition] = useState('');
-  const [questionType, setQuestionType] = useState('technical');
-  const [recordingType, setRecordingType] = useState<'audio' | 'video'>('audio');
+  const [selectedQuestion, setSelectedQuestion] = useState<
+    string | null
+  >(null);
+  const [name, setName] = useState("");
+  const [company, setCompany] = useState("");
+  const [position, setPosition] = useState("");
+  const [questionType, setQuestionType] = useState("technical");
+  const [recordingType, setRecordingType] = useState<
+    "audio" | "video"
+  >("audio");
   const [isQuestionAnswered, setIsQuestionAnswered] = useState(false);
   const [stepVisible, setStepVisible] = useState(true);
 
@@ -24,20 +28,25 @@ const Interview = () => {
   const fetchQuestion = async () => {
     try {
       const response = await axios.get(
-        baseUrl ? `${baseUrl}/service/generate_interview_question` : "/service/generate_interview_question",
+        baseUrl
+          ? `${baseUrl}/service/generate_interview_question`
+          : "/service/generate_interview_question",
         {
           params: {
             name,
             company,
             position,
-            interview_type: questionType
+            interview_type: questionType,
           },
           headers: { "Content-Type": "application/json" },
         }
       );
       setSelectedQuestion(response.data);
     } catch (error) {
-      console.error('Error fetching interview question from Gemini:', error);
+      console.error(
+        "Error fetching interview question from Gemini:",
+        error
+      );
     }
   };
 
@@ -67,10 +76,16 @@ const Interview = () => {
     return (
       <div className="hero">
         <div className="flex-1 pt-36 padding-x">
-          <h1 className="text-2xl font-bold">Interview Meeting Room</h1>
-          <p className="text-lg mt-4">Sorry, but you must be signed in to start your interview.</p>
+          <h1 className="text-2xl font-bold">
+            Interview Meeting Room
+          </h1>
+          <p className="text-lg mt-4">
+            Sorry, but you must be signed in to start your interview.
+          </p>
           <button className="bg-primary-blue text-white mt-10 rounded-full">
-            <a href="/api/auth/login">Sign In to Start Your Interview</a>
+            <a href="/api/auth/login">
+              Sign In to Start Your Interview
+            </a>
           </button>
         </div>
       </div>
@@ -79,11 +94,15 @@ const Interview = () => {
     return (
       <div className="hero items-center sm:flex-row sm:items-start">
         <div className="flex-1 pt-36 padding-x w-full sm:w-1/2">
-          <h1 className="text-2xl font-bold">Interview Meeting Room</h1>
+          <h1 className="text-2xl font-bold">
+            Interview Meeting Room
+          </h1>
 
           {step === 1 && (
-            <div className={`fade-in ${!stepVisible ? 'fade-out' : ''}`}>
-              <div className="flex flex-col mb-6">
+            <div
+              className={`fade-in ${!stepVisible ? "fade-out" : ""}`}
+            >
+              <div className="flex flex-col mb-6 text-black">
                 <label>Your Name:</label>
                 <input
                   type="text"
@@ -92,13 +111,20 @@ const Interview = () => {
                   className="border p-2"
                 />
               </div>
-              <button onClick={handleNextStep} className="bg-primary-blue text-white mt-4 rounded-full p-2">Next</button>
+              <button
+                onClick={handleNextStep}
+                className="bg-primary-blue text-white mt-4 rounded-full p-2"
+              >
+                Next
+              </button>
             </div>
           )}
 
           {step === 2 && (
-            <div className={`fade-in ${!stepVisible ? 'fade-out' : ''}`}>
-              <div className="flex flex-col mb-6">
+            <div
+              className={`fade-in ${!stepVisible ? "fade-out" : ""}`}
+            >
+              <div className="flex flex-col mb-6 text-black">
                 <label>Company:</label>
                 <input
                   type="text"
@@ -107,13 +133,20 @@ const Interview = () => {
                   className="border p-2"
                 />
               </div>
-              <button onClick={handleNextStep} className="bg-primary-blue text-white mt-4 rounded-full p-2">Next</button>
+              <button
+                onClick={handleNextStep}
+                className="bg-primary-blue text-white mt-4 rounded-full p-2"
+              >
+                Next
+              </button>
             </div>
           )}
 
           {step === 3 && (
-            <div className={`fade-in ${!stepVisible ? 'fade-out' : ''}`}>
-              <div className="flex flex-col mb-6">
+            <div
+              className={`fade-in ${!stepVisible ? "fade-out" : ""}`}
+            >
+              <div className="flex flex-col mb-6 text-black">
                 <label>Position:</label>
                 <input
                   type="text"
@@ -122,13 +155,20 @@ const Interview = () => {
                   className="border p-2"
                 />
               </div>
-              <button onClick={handleNextStep} className="bg-primary-blue text-white mt-4 rounded-full p-2">Next</button>
+              <button
+                onClick={handleNextStep}
+                className="bg-primary-blue text-white mt-4 rounded-full p-2"
+              >
+                Next
+              </button>
             </div>
           )}
 
           {step === 4 && (
-            <div className={`fade-in ${!stepVisible ? 'fade-out' : ''}`}>
-              <div className="flex flex-col mb-6">
+            <div
+              className={`fade-in ${!stepVisible ? "fade-out" : ""}`}
+            >
+              <div className="flex flex-col mb-6 text-black">
                 <label>Question Type:</label>
                 <select
                   value={questionType}
@@ -139,12 +179,19 @@ const Interview = () => {
                   <option value="behavioral">Behavioral</option>
                 </select>
               </div>
-              <button onClick={handleNextStep} className="bg-primary-blue text-white mt-4 rounded-full p-2">Next</button>
+              <button
+                onClick={handleNextStep}
+                className="bg-primary-blue text-white mt-4 rounded-full p-2"
+              >
+                Next
+              </button>
             </div>
           )}
 
           {step === 5 && (
-            <div className={`fade-in ${!stepVisible ? 'fade-out' : ''}`}>
+            <div
+              className={`fade-in ${!stepVisible ? "fade-out" : ""}`}
+            >
               <div className="flex flex-col mb-6">
                 <label>Recording Type:</label>
                 <select
@@ -156,30 +203,44 @@ const Interview = () => {
                   <option value="video">Video</option>
                 </select>
               </div>
-              <button onClick={() => setStep(step + 1)} className="bg-primary-blue text-white mt-4 rounded-full p-2">Start Interview</button>
+              <button
+                onClick={() => setStep(step + 1)}
+                className="bg-primary-blue text-white mt-4 rounded-full p-2"
+              >
+                Start Interview
+              </button>
             </div>
           )}
 
           {step === 6 && selectedQuestion && (
-            <div className={`fade-in ${!stepVisible ? 'fade-out' : ''}`}>
-              <AnalysisCard analysis={[selectedQuestion]} title="Interview Question Provided by mockAI" />
-              {recordingType === 'audio' ? (
+            <div
+              className={`fade-in ${!stepVisible ? "fade-out" : ""}`}
+            >
+              <AnalysisCard
+                analysis={[selectedQuestion]}
+                title="Interview Question Provided by mockAI"
+              />
+              {recordingType === "audio" ? (
                 <VoiceRecorder
                   selectedQuestion={selectedQuestion}
                   user={user}
-                  onRecordingComplete={() => setIsQuestionAnswered(true)}
+                  onRecordingComplete={() =>
+                    setIsQuestionAnswered(true)
+                  }
                 />
               ) : (
                 <VideoRecorder
                   selectedQuestion={selectedQuestion}
                   user={user}
-                  onRecordingComplete={() => setIsQuestionAnswered(true)}
+                  onRecordingComplete={() =>
+                    setIsQuestionAnswered(true)
+                  }
                 />
               )}
             </div>
           )}
           <button className='"bg-primary-blue text-white mt-4 rounded-full p-2'>
-            <Link href='/results'>See Your Results</Link>
+            <Link href="/results">See Your Results</Link>
           </button>
         </div>
       </div>
