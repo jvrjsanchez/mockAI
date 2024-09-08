f71e35d7339979ba2e334dc885c47cc84c11dad2
Tim Quattrochi <timq82@gmail.com>
2024-09-08 17:37:32 -0400

    refactor: change fill color

--- mock_ai/components/ui/progress.tsx
+++ mock_ai/components/ui/progress.tsx
@@ -18,7 +18,7 @@ const Progress = React.forwardRef<
     {...props}
   >
     <ProgressPrimitive.Indicator
-      className="h-full w-full flex-1 bg-primary transition-all"
+      className="h-full w-full flex-1 bg-gradient-to-r from-[#7fceff] to-[#ff6db3] transition-all"
       style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
     />
   </ProgressPrimitive.Root>
