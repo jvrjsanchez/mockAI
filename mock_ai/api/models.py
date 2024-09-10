0af706b5c9c34c09d60d0f151118ab8d8327c872
Tim Quattrochi <timq82@gmail.com>
2024-09-10 18:15:44 -0400

    chore: add colums to Question table

--- mock_ai/api/models.py
+++ mock_ai/api/models.py
@@ -13,6 +13,10 @@ class Question(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     question = db.Column(db.String, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    company = db.Column(db.String, nullable=False)
+    position = db.Column(db.String, nullable=False)
+    interview_type = db.Column(db.String, nullable=False)
 
 
 class Result(db.Model):
@@ -22,7 +26,6 @@ class Result(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     question_id = db.Column(db.Integer, db.ForeignKey(
         'questions.id'), nullable=False)
-    question = db.Column(db.String, nullable=False)
     updated_at = db.Column(
         db.DateTime, nullable=True, default=db.func.now()
     )
@@ -41,8 +44,10 @@ class Result(db.Model):
         'Question', backref=db.backref('results', lazy=True))
 
     # method to return the result as a dictionary so we can jsonify it.
+    # question=self.question_rel.question  is used to get the question from the question table using the question_id foreign key.
+
     def get_as_dict(self) -> dict:
-        result_dict = dict(id=self.id, user_id=self.user_id, question_id=self.question_id, question=self.question, updated_at=self.updated_at, score=self.score, transcript=self.transcript,
+        result_dict = dict(id=self.id, user_id=self.user_id, question_id=self.question_id, question=self.question_rel.question, updated_at=self.updated_at, score=self.score, transcript=self.transcript,
                            filler_words=self.filler_words, long_pauses=self.long_pauses, pause_durations=self.pause_durations, ai_feedback=self.ai_feedback, audio_url=self.audio_url, video_url=self.video_url, interview_date=self.interview_date)
 
         return result_dict
